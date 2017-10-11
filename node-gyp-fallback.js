d41ec0416de9ba8ae719d6cdf23acedad981a087
Dmitri Melikyan <dmitri@stackimpact.com>
2017-10-11 11:26:51 +0200

    pre-built addons for linux with node-gyp fallback

---
+++ node-gyp-fallback.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const os = require('os');
+const child_process = require('child_process')
+
+
+let addonPath = `./prebuilt/${os.platform()}/${process.arch}/${process.version}/stackimpact-addon.node`;
+try {
+  if (fs.statSync(addonPath).isFile()) {
+    console.log('Pre-built version of StackImpact addon found, not building.');
+    return;
+  }
+}
+catch(err) {
+  console.log('Pre-built version of StackImpact addon not found, trying to build.');
+}
+
+let gyp = child_process.spawn('node-gyp', ['rebuild'], {cwd: process.cwd(), env: process.env, stdio: 'inherit'});
+
+gyp.on('error', (err) => {
+  process.exit(1);
+});
+
+gyp.on('close', (code) => {
+  process.exit(code);
+});
